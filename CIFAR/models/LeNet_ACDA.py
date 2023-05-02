0189bdec4f2059bb98481c14b9246763e284d224
PengfeiDeng <75402192+PengfeiDeng@users.noreply.github.com>
2023-05-01 22:19:48 -0400

    Update LeNet_ACDA.py

--- CIFAR/models/LeNet_ACDA.py
+++ CIFAR/models/LeNet_ACDA.py
@@ -3,13 +3,13 @@ import torch.nn.functional as F
 from ACDA.Conv_DCFD import *
 
 class LeNet_ACDA(nn.Module):
-    def __init__(self):
+    def __init__(self,out_size):
         super(LeNet_ACDA, self).__init__()
         self.conv1 = Conv_DCFD(3, 6, kernel_size=5)
         self.conv2 = Conv_DCFD(6, 16, kernel_size=5)
         self.fc1 = nn.Linear(1024, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, out_size)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
